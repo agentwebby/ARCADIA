9e70e000110e84adfb1c51adf815134f07e26ef1
oracle <webbyhj@gmail.com>
2025-04-22 04:48:31 +0530

    ui done

--- src/app/ui/submitButton.tsx
+++ src/app/ui/submitButton.tsx
@@ -4,6 +4,7 @@ import { useFormStatus } from 'react-dom'
 export default function SubmitButton() {
   const status = useFormStatus()
   return (
-    <button type="submit">{status.pending ? 'Searching...' :''}Submit</button>
+    <button className="text-teal-700 hover:text-white border border-teal-700 hover:bg-teal-800 focus:ring-4 focus:outline-none focus:ring-teal-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2 dark:border-teal-400 dark:text-teal-400 dark:hover:text-white dark:hover:bg-teal-500 dark:focus:ring-teal-900"
+            type="submit">{status.pending ? 'Searching...' :''}Submit</button>
   )
 }

f25809eff17d35ba3d40d057f52120f399afacef
oracle <webbyhj@gmail.com>
2025-04-27 00:52:58 +0530

    choose file button added

--- src/app/ui/form1.tsx
+++ src/app/ui/form1.tsx
@@ -3,10 +3,9 @@ import Form from 'next/form'
 import SubmitButton from './submitButton'
 export default function Form1() {
   return (
-    <Form action="/search">
-      {/* On submission, the input value will be appended to
-          the URL, e.g. /search?query=abc */}
-      <input name="query" />
+    <Form action="/upload" formMethod='POST' formEncType='multipart/form-data' >
+      
+      <input type = "file" name = 'file' />
       <br/>
       <SubmitButton></SubmitButton>
     </Form>
